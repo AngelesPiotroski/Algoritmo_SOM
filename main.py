7e39234d6c621399db80e0829f81a289ccd0f889
Damian Stetson <damianstetson17@gmail.com>
2021-04-09 09:05:42 -0300

    plot function added

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 # need >pip install matplotlib
 # encoding: utf-8
 import functions
+import plot_functions
 
 def main():
   #read the knowledge and save it in to a list of vectors:
   data_file_txt = open("data_r2.txt", "r")
   list_points_Knowledge=functions.load_from_txt(data_file_txt)
-  
+
   #testing if the function list_points_Knowledge is returning a list from txt:
   #print(list_points_Knowledge)
   
   #testing the function closest_weight with a  Point[0,1] and list_points_Knowledge:
   #print("el nodo mas cercano a [0,1] es: ", functions.closest_weight([0,1],list_points_Knowledge))
 
+  #start algorithm
   number_of_clusters = 2
   clustersList = []
   MaxStep=1000
   t=1
-  ######start algorithm######
+ 
   #genering random values for the centroides at first time
   clustersList = functions.generate_random_list(number_of_clusters)
-  
+
+  #plot the initial values
+  plot_functions.plot_r2(list_points_Knowledge,clustersList)
   
   while( t < MaxStep):
     
---
+++ plot_functions.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+import Tkinter
+# importar todas las funciones de pylab
+from pylab import *
+
+# importar el módulo pyplot
+import matplotlib.pyplot as plt
+
+def plot_r2(points_to_plot,centroides_to_plot):
+    #plot atributes
+    title('Valores cargados:')
+    xlabel('Eje X')
+    ylabel('Eje Y')
+    #x list from points
+    xArrayPoints = [point[0] for point in points_to_plot]
+    #y list from points
+    yArrayPoints= [point[1] for point in points_to_plot]
+    #make a plot
+    scatter(xArrayPoints,yArrayPoints, label='Puntos Conocimiento')
+    #x list from centroides
+    xArrayCentroide = [weight[0] for weight in centroides_to_plot]
+    #x list from centroides
+    yArrayCentroide= [weight[1] for weight in centroides_to_plot]
+    #make a plot
+    scatter(xArrayCentroide,yArrayCentroide, label='Pesos')
+    #make legend
+    plt.legend(loc='upper left')
+    show()
+
