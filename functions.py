d09c24febb040ae35e4fd527e8e8523927f49669
Angeles Piotroski <piotroskiangeles@gmail.com>
2021-04-21 23:27:31 -0300

    creacion del algoritmo en el main, modificacion de la funcion vecindad y actualizacion pesos

--- functions.py
+++ functions.py
@@ -50,31 +50,38 @@ def closest_weight(point, centroides):
     allResultsEuclidean = []
     #for any centroide calculate the euclidean distance
     for w in centroides:
-        print("calculating eucliden distance of", w , " = ", euc_distance(point,w)," and added to list of results")
+        #print("calculating eucliden distance of", w , " = ", euc_distance(point,w)," and added to list of results")
         allResultsEuclidean.append(euc_distance(point,w))
-    print("\nthe min in the list of results is: ", min(allResultsEuclidean))
+    #print("\nthe min in the list of results is: ", min(allResultsEuclidean))
     #catching the index of the min result in allResultsEuclidean list (this tells us which element of the list of centroids is the minimum)
     indexOfClosest = allResultsEuclidean.index(min(allResultsEuclidean))
-    print("the position of the min is: ", indexOfClosest, " (positions in vectors starts in cero)")
+    #print("the position of the min is: ", indexOfClosest, " (positions in vectors starts in cero)")
     #returning the centroide in the index of min result in allResultsEuclidean
     return (centroides[indexOfClosest])
 
 #return the difference between two points in R2
-def diff_r2(point1,point2):
+def diff_r2(x1,x2):
     #this is like 'x1-x1', 'y1-y2' and return a vector with this result
-    diff_result=[point1[0]-poin2[0],point1[1]-poin2[1]]
+    diff_result=x1-x2
     return diff_result
 
 #return the value from evaluate the function
-def vicinityImpactFunction(centroideWeightWinner,centroideToUpdate):
+def vicinityImpactFunctionInX(centroideWeightWinner,centroideToUpdate):
     #1 con la ganadora >1 <0 si no es la ganadora.  vecindad entre conjunto de pesos distancia entre conuntos de pesos y la ganadora
-    #return (1/(1+diff_r2(centroideToUpdate,centroideWeightWinner)))
-    return (1/(1+euc_distance(centroideToUpdate,centroideWeightWinner)))
+    return (1/(1+diff_r2(centroideToUpdate[0],centroideWeightWinner[0])))
+    #return (1/(1+euc_distance(centroideToUpdate,centroideWeightWinner)))
+
+#return the value from evaluate the function
+def vicinityImpactFunctionInY(centroideWeightWinner,centroideToUpdate):
+    #1 con la ganadora >1 <0 si no es la ganadora.  vecindad entre conjunto de pesos distancia entre conuntos de pesos y la ganadora
+    return (1/(1+diff_r2(centroideToUpdate[1],centroideWeightWinner[1])))
+    #return (1/(1+euc_distance(centroideToUpdate,centroideWeightWinner)))
+
 
 #return the centroide updated with the new values
-def update_weight(centroide, pointKnow, vecinityFuncValue,LearnRestrictor):
+def update_weight(centroideWeightWinner, pointKnow, vecinityFuncValueX,vecinityFuncValueY,LearnRestrictor):
     #apply the formula for update weights
-    weight_x=centroideWeightWinner[0]+vecinityFuncValue*LearnRestrictor*(pointKnow[0]-centroideWeightWinner[0])
-    weight_y=centroideWeightWinner[1]+vecinityFuncValue*LearnRestrictor*(pointKnow[1]-centroideWeightWinner[1])
-    centroide_weight_Updated=(weight_x,weight_y)
+    weight_x=centroideWeightWinner[0]+vecinityFuncValueX*LearnRestrictor*(pointKnow[0]-centroideWeightWinner[0])
+    weight_y=centroideWeightWinner[1]+vecinityFuncValueY*LearnRestrictor*(pointKnow[1]-centroideWeightWinner[1])
+    centroide_weight_Updated=(round(weight_x,2),round(weight_y,2))
     return centroide_weight_Updated
--- main.py
+++ main.py
@@ -15,25 +15,45 @@ def main():
   #print("el nodo mas cercano a [0,1] es: ", functions.closest_weight([0,1],list_points_Knowledge))
 
   #start algorithm
-  number_of_clusters = 2
-  clustersList = []
-  MaxStep=1000
-  t=1
- 
-  #genering random values for the centroides at first time
-  clustersList = functions.generate_random_list(number_of_clusters)
 
-  #plot the initial values
-  #plot_functions.plot_r2(list_points_Knowledge,clustersList)
-  
+  clustersList = []
+  MaxStep=3000
+  t=1
+  LearnRestrictor=1
+  #take the number of clusters
+  number_of_clusters= 10
+  #generate the clusters
+  clustersList = functions.generate_random_list(number_of_clusters)
+  listaClusterInicial= clustersList
   while( t < MaxStep):
     #just for test
-    clustersList = functions.generate_random_list(number_of_clusters)
-    plot_functions.plot_r2(list_points_Knowledge,clustersList)
-    
+    #plot_functions.plot_r2(list_points_Knowledge,clustersList)
+    #for each known point in the data list
+    for point in list_points_Knowledge:
+      #get the winning centroid
+      winning_centroid = functions.closest_weight(point,clustersList)
+      #for each cluster in the cluster list
+      for centroide in clustersList:
+        #calculate the vicinity function
+        vecinity_value_X= functions.vicinityImpactFunctionInX(winning_centroid,centroide )
+        vecinity_value_Y= functions.vicinityImpactFunctionInY(winning_centroid,centroide )
+        round(vecinity_value_X,2)
+        round(vecinity_value_Y,2)
+        #update the weights of each cluster
+        #print("el viejo valor del centroide es ",centroide)
+        centroide= functions.update_weight(winning_centroid , point, vecinity_value_X,vecinity_value_Y , LearnRestrictor)
+        #print("el nuevo valor del centroide es ",centroide)
+        
+    #show iteration
     t+=1
     LearnRestrictor=1/t
+    print("####################----------------------------------------iteracion nro ", t)
+    print("####################----------------------------------------el lear es  ", LearnRestrictor)
+
   #end algorithm
+  plot_functions.plot_r2(list_points_Knowledge,clustersList)
+  print("los valores iniciales de los clusters son ",listaClusterInicial)
+  print("los valores finales de los clusters son ",clustersList)
   
 if __name__=="__main__":
   main()
